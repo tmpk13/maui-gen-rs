f2ffa8019ee8c9952f23ba449f4cd7f30a83eca9
tmpk13 <167717442+tmpk13@users.noreply.github.com>
2026-02-04 12:06:26 -0800

    Add constants

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
+use ratatui::Frame;
+use ratatui::layout::Rect;
+use ratatui::style::{Style, Stylize};
+use ratatui::widgets::{Block, List, ListDirection, ListItem};
+
+const PROJECT_NAME: &str = "";
+
+const VIEW_PATH: &str = "";
+const VIEW_MODEL_PATH: &str = "";
+
+const NEW_VIEW: &str = "";
+const NEW_VIEW_MODEL: &str = "";
+
 fn main() {
-    println!("Hello, world!");
+
 }
